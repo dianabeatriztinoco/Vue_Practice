ddbb067e45910c26a4c8ac995136e09fbbea7955
dtinoco <dtinoco@demeterlogistics.com>
2021-12-09 17:01:20 -0600

    added method to calculate age in five years

--- basics-assignment-1-problem/app.js
+++ basics-assignment-1-problem/app.js
@@ -3,11 +3,14 @@ const app = Vue.createApp({
         return {
             myName: "Diana Beatriz Tinoco",
             myAge:32,
-            futureAge:37,
             someImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpTmGSKs1Vrtl80YlFj1e2IcKnl9sY9catUZG9-kaPKItt10Y9glsxS4ZWvmuZrBV-DjU&usqp=CAU"
         }
     },
     methods: {
+
+        futureAge(){
+            return this.myAge + 5
+        },
         randomNumber(){
             return Math.random()
         }
