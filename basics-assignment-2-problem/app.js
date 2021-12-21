f18fff8c10d95a6adae1d5bc9196cb2c6e183660
dtinoco <dtinoco@demeterlogistics.com>
2021-12-21 09:18:19 -0600

    d

---
+++ basics-assignment-2-problem/app.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+
+data() {
+    return {
+        confirmedUserInputKeyDown: "", 
+        confirmedUserInputEnter: ""
+    }
+},
+methods: {
+
+    showAlert(){
+        alert("i've been clicked!")
+    },
+
+    userInput(event){
+        this.confirmedUserInputKeyDown = event.target.value
+        
+    }, 
+
+    userInputEnter(event){
+        this.confirmedUserInputEnter = event.target.value
+    }
+
+
+},
+
+})
+
+app.mount("#assignment")
